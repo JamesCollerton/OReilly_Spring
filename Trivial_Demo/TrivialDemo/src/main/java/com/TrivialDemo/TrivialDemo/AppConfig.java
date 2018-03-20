7991376056fcd5c739f98ae33df46571a5af2ee6
James Collerton <James.Collerton@kobaltmusic.com>
2018-03-20 15:26:01 +0000

    Did it so teams were autowired

--- Trivial_Demo/TrivialDemo/src/main/java/com/TrivialDemo/TrivialDemo/AppConfig.java
+++ Trivial_Demo/TrivialDemo/src/main/java/com/TrivialDemo/TrivialDemo/AppConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
@@ -20,11 +21,15 @@ public class AppConfig {
 
     /**
      * This goes to find the bean in whichever config file this exists in. Note,
-     * this only works if there is one data source
+     * this only works if there is one data source. Note, Autowired is autowired
+     * by type.
      */
     @Autowired
     private DataSource dataSource;
 
+    /**
+     * Note, in general you should put beans you want to scan into the same folder
+     */
     @Autowired @Qualifier("redSox")
     private Team home;
 
