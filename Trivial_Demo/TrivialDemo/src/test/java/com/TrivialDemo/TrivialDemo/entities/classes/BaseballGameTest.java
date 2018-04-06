5d40b0c6eae3197691471132c8569acdaa686716
James Collerton <James.Collerton@kobaltmusic.com>
2018-04-06 16:29:50 +0100

    Autowired in the test context and the game

--- Trivial_Demo/TrivialDemo/src/test/java/com/TrivialDemo/TrivialDemo/entities/classes/BaseballGameTest.java
+++ Trivial_Demo/TrivialDemo/src/test/java/com/TrivialDemo/TrivialDemo/entities/classes/BaseballGameTest.java
@@ -5,23 +5,25 @@ import com.TrivialDemo.TrivialDemo.entities.interfaces.Game;
 import com.TrivialDemo.TrivialDemo.entities.interfaces.Team;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
 public class BaseballGameTest {
 
+    @Autowired
     private Game game;
 
+    @Autowired
     private ApplicationContext ctx;
-
-    @Before
-    public void setUp() {
-        ctx = new AnnotationConfigApplicationContext(AppConfig.class);
-        game = ctx.getBean("game", Game.class);
-    }
 
     @Test
     public void testPlayGame() throws Exception {
