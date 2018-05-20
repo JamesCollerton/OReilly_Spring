4d8bcd4f4a1e5038771dc7a58150ce54495801d7
James Collerton <jamescollerton@live.co.uk>
2018-05-20 15:58:37 +0100

    Written interface to get by customerId and mpxn

--- MeterReads/MeterReads/src/main/java/com/MeterReads/MeterReads/Services/Repositories/MeterReadingRepository.java
+++ MeterReads/MeterReads/src/main/java/com/MeterReads/MeterReads/Services/Repositories/MeterReadingRepository.java
@@ -3,10 +3,10 @@ package com.MeterReads.MeterReads.Services.Repositories;
 import org.springframework.data.repository.CrudRepository;
 import com.MeterReads.MeterReads.DataObjects.Entities.MeterReading;
 
+import java.util.List;
+
 public interface MeterReadingRepository extends CrudRepository<MeterReading, Long> {
 
-//    MeterReading findByFirstName(String firstName);
-//
-//    List<MeterReading> findByLastName(String lastName);
+    List<MeterReading> findByCustomerIdAndMpxn(String customerId, long mpxn);
 
 }
