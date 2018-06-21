a4ea03ea42f34c0d4adc179ff8e6790f9e5bf28a
James Collerton <jamescollerton@live.co.uk>
2018-06-21 07:44:26 +0100

    Probably all working, but need to actually create a meter reading object

--- MeterReads/MeterReads/src/test/java/com/MeterReads/MeterReads/Controllers/MeterReadingAcceptanceControllerTest.java
+++ MeterReads/MeterReads/src/test/java/com/MeterReads/MeterReads/Controllers/MeterReadingAcceptanceControllerTest.java
@@ -1,22 +1,42 @@
 package com.MeterReads.MeterReads.Controllers;
 
+import com.MeterReads.MeterReads.DataObjects.Entities.MeterReading;
+import com.MeterReads.MeterReads.MeterReadsApplication;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.spy;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = MeterReadsApplication.class)
 @WebAppConfiguration
 public class MeterReadingAcceptanceControllerTest {
 
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
     private MockMvc mockMvc;
+
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -27,11 +47,21 @@ public class MeterReadingAcceptanceControllerTest {
     }
 
     @Test
-    public void test() {
-//        mockMvc.perform(post("/george/bookmarks/")
-//                .content(this.json(new Bookmark(null, null, null)))
-//                .contentType(contentType))
-//                .andExpect(status().isNotFound());
+    public void test() throws Exception {
+
+        MeterReading meterReading = spy(MeterReading.class);
+
+        mockMvc.perform(post("/meter-read")
+                .content(this.json(meterReading))
+                .contentType(contentType))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+    private String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
     }
 
 }
