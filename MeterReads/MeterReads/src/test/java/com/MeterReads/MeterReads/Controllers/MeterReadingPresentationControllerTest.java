8d8bd50f087adc486f5daed4ac8df7977d6e0f3e
James Collerton <James.Collerton@kobaltmusic.com>
2018-06-25 09:28:00 +0100

    Changed the integration tests slightly

--- MeterReads/MeterReads/src/test/java/com/MeterReads/MeterReads/Controllers/MeterReadingPresentationControllerTest.java
+++ MeterReads/MeterReads/src/test/java/com/MeterReads/MeterReads/Controllers/MeterReadingPresentationControllerTest.java
@@ -1,6 +1,7 @@
 package com.MeterReads.MeterReads.Controllers;
 
 import com.MeterReads.MeterReads.MeterReadsApplication;
+import com.MeterReads.MeterReads.Services.Repositories.MeterReadingRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,11 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -27,14 +30,21 @@ public class MeterReadingPresentationControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
+    @Autowired
+    private MeterReadingRepository meterReadingRepository;
+
     @Before
     public void setup() throws Exception {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
+
+        this.meterReadingRepository.deleteAll();
     }
 
     @Test
-    public void meterRead_ValidRequestNoData_ExecutesReturnsNoData() {
-
+    public void meterRead_ValidRequestNoData_ExecutesReturnsNoData() throws Exception {
+        mockMvc.perform(get("/meter-read/customerIds/{customerId}/serialNumbers/{serialNumber}", "customerId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
     }
 
     @Test
@@ -60,12 +70,6 @@ public class MeterReadingPresentationControllerTest {
     @Test
     public void meterRead_InvalidRequestInvalidURI_ExecutesReturnsErrorCode() {
 
-    }
-
-    @Test
-    public void test() throws Exception {
-        mockMvc.perform(get("/meter-read/customerIds/{customerId}/serialNumbers/{serialNumber}", "1", "2"))
-                .andDo(print()).andExpect(status().isOk());
     }
 
 }
