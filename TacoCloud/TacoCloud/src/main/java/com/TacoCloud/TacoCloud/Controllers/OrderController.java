58fec89fbf54c7067061fc285a2cd908cb3779ee
James Collerton <James.Collerton@kobaltmusic.com>
2018-09-05 16:21:32 +0100

    Got design and order routed and working

--- TacoCloud/TacoCloud/src/main/java/com/TacoCloud/TacoCloud/Controllers/OrderController.java
+++ TacoCloud/TacoCloud/src/main/java/com/TacoCloud/TacoCloud/Controllers/OrderController.java
@@ -3,6 +3,7 @@ package com.TacoCloud.TacoCloud.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 import com.TacoCloud.TacoCloud.Domain.Entities.Order;
@@ -16,6 +17,12 @@ public class OrderController {
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+
+    @PostMapping()
+    public String processOrder(Order order) {
+        log.info("Order submitted: " + order);
+        return "redirect:/";
     }
 
 }
