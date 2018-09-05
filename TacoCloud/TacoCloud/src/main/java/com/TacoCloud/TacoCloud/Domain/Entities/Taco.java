03a68814ecc54c4491346580802bf3f10167e0d1
James Collerton <James.Collerton@kobaltmusic.com>
2018-09-05 16:34:33 +0100

    Added Java Bean validation

--- TacoCloud/TacoCloud/src/main/java/com/TacoCloud/TacoCloud/Domain/Entities/Taco.java
+++ TacoCloud/TacoCloud/src/main/java/com/TacoCloud/TacoCloud/Domain/Entities/Taco.java
@@ -1,12 +1,21 @@
 package com.TacoCloud.TacoCloud.Domain.Entities;
 
 import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Data;
 
+/**
+ * Contains validation rules as part of the Java Bean validation API
+ */
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String       name;
+
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<String> ingredients;
 
 }
