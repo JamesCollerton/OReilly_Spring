13b35ffa60c68e5c67c04f7457617f074e7ed42c
James Collerton <James.Collerton@kobaltmusic.com>
2018-09-12 12:26:55 +0100

    Finished chapter three

--- TacoCloud/TacoCloud/src/main/java/com/TacoCloud/TacoCloud/Repositories/OrderRepository.java
+++ TacoCloud/TacoCloud/src/main/java/com/TacoCloud/TacoCloud/Repositories/OrderRepository.java
@@ -2,7 +2,6 @@ package com.TacoCloud.TacoCloud.Repositories;
 
 import java.util.Date;
 import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.TacoCloud.TacoCloud.Domain.Entities.Order;
 
